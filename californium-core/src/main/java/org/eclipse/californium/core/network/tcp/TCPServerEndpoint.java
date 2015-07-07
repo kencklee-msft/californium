804600171131f14f758fc7194f3a91fe05f031ef
LDaPrato <LDaPrato@zebra.com>
2015-07-07 11:35:53 -0500

    Removed connection state listener from client.

--- californium-core/src/main/java/org/eclipse/californium/core/network/tcp/TCPServerEndpoint.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/tcp/TCPServerEndpoint.java
@@ -1,7 +1,6 @@
 package org.eclipse.californium.core.network.tcp;
 
 import java.net.InetSocketAddress;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -10,41 +9,38 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.network.CoAPEndpoint;
+import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.elements.tcp.ConnectionInfo;
 import org.eclipse.californium.elements.tcp.ConnectionInfo.ConnectionState;
 import org.eclipse.californium.elements.tcp.ConnectionStateListener;
+import org.eclipse.californium.elements.tcp.server.TcpServerConnector;
 
-/**
- * This class is meant for server TCP connection only
- *
- * If you build a TCP server that must serve to multiple TCP Clients containing the coapServer (hence multiple CoAP Client from 1 TCP connection)
- * You must use this registry to keep track of your clients. Once a Connection is established, the call will be made for you to create a CoAP client
- * if you wish to do so.
- */
-public abstract class CoapClientRegistry implements ConnectionStateListener {
+public abstract class TCPServerEndpoint extends CoAPEndpoint implements ConnectionStateListener {
 
-	private final static Logger LOGGER = Logger.getLogger(CoapClientRegistry.class.getCanonicalName());
+	private final static Logger LOGGER = Logger.getLogger(TCPServerEndpoint.class.getCanonicalName());
 
 	public final Map<InetSocketAddress, CoapClient> coapClientLink;
-	private final TCPEndpoint endpoint;
 
-
-	public CoapClientRegistry(final boolean threadsafe, final TCPEndpoint endpoint) {
-		coapClientLink = threadsafe ? new ConcurrentHashMap<InetSocketAddress, CoapClient>() : new HashMap<InetSocketAddress, CoapClient>();
-		this.endpoint = endpoint;
-		endpoint.addConnectionStateListener(this);
+	/**
+	 * Instantiates a new endpoint with the specified connector and
+	 * configuration.
+	 *
+	 * @param connector the connector
+	 * @param config the config
+	 */
+	public TCPServerEndpoint(final TcpServerConnector connector, final NetworkConfig config) {
+		super(connector, config);
+		coapClientLink = new ConcurrentHashMap<InetSocketAddress, CoapClient>();
+		connector.setConnectionStateListener(this);
 	}
 
 	@Override
 	public void stateChange(final ConnectionInfo info) {
 		LOGGER.log(Level.FINE, "Incoming connection state change from " + info.getRemote() + " to  " + info.getConnectionState());
-		if(endpoint == null) {
-			LOGGER.log(Level.FINE, "No Endpoint setup, will not trigger creation of Client");
-			return;
-		}
 		if(info.getConnectionState().equals(ConnectionState.NEW_INCOMING_CONNECT)) {
 			final CoapClient client = new CoapClient();
-			client.setEndpoint(endpoint);
+			client.setEndpoint(this);
 			final InetSocketAddress key = info.getRemote();
 			configureCoapClient(client, key);
 			coapClientLink.put(key, client);
@@ -84,4 +80,5 @@ public abstract class CoapClientRegistry implements ConnectionStateListener {
 	 * @param endpoint
 	 */
 	public abstract void configureCoapClient(CoapClient client, InetSocketAddress remote);
+
 }
